beb1d61d5672ce38cb5b28790b50bf8446b2720f
somerandomguy101 <c3029594@newcastle.ac.uk>
2026-02-28 13:34:50 +0000

    created 2 basic test files for the networkx implementation

--- base nx EPB/edge_bundling.py
+++ base nx EPB/edge_bundling.py
@@ -2,17 +2,18 @@ import networkx as nx
 
 def edge_path_bundling(G, k, d):
 
-    lock = {e: False for e in G.edges}
-    skip = {e: False for e in G.edges}
+    lock = {tuple(sorted(e)): False for e in G.edges}
+    skip = {tuple(sorted(e)): False for e in G.edges}
 
     weight = {
-        e: (G.edges[e]["length"] ** d) for e in G.edges
+        tuple(sorted(e)): (G.edges[e]["length"] ** d) for e in G.edges
     }
 
     sorted_edges = sorted(G.edges, key=lambda e: weight[e], reverse=True)
     control_points = {}
 
     for e in sorted_edges:
+        e = tuple(sorted(e))
         if lock[e]:
             continue
 
@@ -34,14 +35,14 @@ def edge_path_bundling(G, k, d):
         # convert node path -> edge path
         path_edges = list(zip(path_nodes[:-1], path_nodes[1:]))
 
-        path_length = sum(weight[m] for m in path_edges)
+        path_length = sum(weight[tuple(sorted(m))] for m in path_edges)
 
         if path_length > k * G.edges[e]["length"]:
             skip[e] = False
             continue
 
         for m in path_edges:
-            lock[m] = True
+            lock[tuple(sorted(m))] = True
 
         control_points[e] = path_nodes
 
---
+++ base nx EPB/testnx2.py
@@ -0,0 +1,58 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import math
+from edge_bundling import edge_path_bundling
+from scipy.interpolate import make_interp_spline
+import numpy as np
+
+G = nx.Graph()
+
+pos = {
+    "A": (0, 0),
+    "B": (1, 3),
+    "C": (3, 6),
+    "D": (5, 8),
+    "E": (7, 10),
+    "F": (9, 11),
+    "G": (12, 12),
+}
+
+def dist(a,b):
+    return math.hypot(a[0]-b[0], a[1]-b[1])
+
+edges = [
+    ("A", "B"), ("B", "C"), ("C", "D"), ("D", "E"), ("E", "F"), ("F", "G"), ("A", "G"), ("C", "F")
+]
+
+for u,v in edges:
+    G.add_edge(u,v,length=dist(pos[u],pos[v]))
+
+bundles = edge_path_bundling(G, 2, 1)
+
+# draw original graph
+nx.draw(G,pos,with_labels=True,edge_color="lightgray", node_color="black", font_color="white")
+
+# draw bundled edges
+for e,path in bundles.items():
+    xs=[pos[n][0] for n in path]
+    ys=[pos[n][1] for n in path]
+
+    # only spline if a path has enough points
+    if len(xs) >=3:
+        t = np.linspace(0,1,len(xs))
+        t_smooth = np.linspace(0,1,100)
+
+        spl_x = make_interp_spline(t, xs, k=2)
+        spl_y = make_interp_spline(t, ys, k=2)
+
+        xs_smooth = spl_x(t_smooth)
+        ys_smooth = spl_y(t_smooth)
+
+        plt.plot(xs_smooth, ys_smooth, color="black")
+
+    else:
+        plt.plot(xs,ys, color="black")
+
+plt.title("Bundled Graph")
+plt.axis("equal")
+plt.show()
