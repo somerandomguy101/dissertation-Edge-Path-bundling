80ddee43496bb4f7d41a0d14d643d4ffbe654ac3
somerandomguy101 <c3029594@newcastle.ac.uk>
2026-03-31 14:32:42 +0100

    removed unnecessary test files and program files

--- base nx EPB/airline.py
+++ base nx EPB/airline.py
@@ -15,4 +15,4 @@ with open("airlines.nodes", "r") as f:
         G.add_node(n, pos=(float(x), float(y)))
 
 # bundling strength ~0.7 or higher recommended
-draw_bundle(G, 2, 2, highlight_node=None, draw_orig=False, bundle_strength=0.7)
+draw_bundle(G, 2, 2, highlight_node=None, draw_orig=False, bundle_strength=0.7, highlight_radius=2.5)
